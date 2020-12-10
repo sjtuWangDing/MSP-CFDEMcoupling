dd03497a9c097cdc3393bfe12f4b42e8d0c97332
sjtuWangDing <2941726668@qq.com>
2020-12-10 21:01:35 +0800

    [src] add string_utils.h and traits.h

---
+++ src/base/string_utils.h
@@ -0,0 +1,47 @@
+/*!
+ * Copyright (c) 2020 by Contributors
+ * @file traits.H
+ * \brief Basic functions of string utils.
+ *
+ * @author Wang Ding
+ */
+ 
+#ifndef __STRING_UTILS_H__
+#define __STRING_UTILS_H__
+
+#include <string>
+#include <sstream>
+
+inline void makeStringKernel(std::stringstream& ss) {}
+
+template<typename DType>
+inline void makeStringKernel(std::stringstream& ss, const DType& data) {
+  ss << data;
+}
+
+template<typename DType, typename... ATypes>
+inline void makeStringKernel(std::stringstream& ss,
+                             const DType& data,
+                             const ATypes&... args) {
+  makeStringKernel(ss, data);
+  makeStringKernel(ss, args...);
+}
+
+template<typename... ATypes>
+std::string makeString(const ATypes&... args) {
+  std::stringstream ss;
+  makeStringKernel(ss, args...);
+  return std::string(ss.str());
+}
+
+//! \brief Specializations for std::string type.
+template<>
+inline std::string makeString(const std::string& str) {
+  return str;
+}
+
+inline std::string makeString(const char* cstr) {
+  return std::string(cstr);
+}
+
+#endif // __STRING_UTILS_H__
---
+++ src/base/traits/traits.h
@@ -0,0 +1,29 @@
+/*!
+ * Copyright (c) 2020 by Contributors
+ * @file traits.H
+ * \brief Traits class for primitives.
+ * \note All primitives need a specialised version of this class. The
+         specialised version will normally also require a conversion method.
+ *
+ * @author Wang Ding
+ */
+
+#ifndef __TRAITS_H__
+#define __TRAITS_H__
+
+#include <iostream>
+
+namespace base {
+
+template<typename PrimitiveType>
+class Traits: public PrimitiveType {
+public:
+  //! \brief Construct from primitive type
+  Traits(const PrimitiveType& p): PrimitiveType(p) {}
+  //! \brief Construct from Istream
+  Traits(std::istream& is): PrimitiveType(is) {}
+};
+
+} // namespace base
+
+#endif // __TRAITS_H__
