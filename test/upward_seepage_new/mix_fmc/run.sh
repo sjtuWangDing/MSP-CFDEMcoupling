8191a600e9f0c13f18813e1764f42a36f70cfd6d
sjtuWangDing <2941726668@qq.com>
2021-02-26 17:11:52 +0800

    [test] update run.sh in mix_fmc

--- test/upward_seepage_new/mix_fmc/run.sh
+++ test/upward_seepage_new/mix_fmc/run.sh
@@ -23,15 +23,33 @@ source $MSP_CFDEM_TOOLS_DIR/msp_cfdem_parallel_run.sh
 casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 solverDir="$MSP_CFDEM_BIN_DIR"
 solverName="mspCfdemSolverMix"
-numberOfProcs="8"
+numberOfProcsDEM="16"
+numberOfProcsCFD="8"
 machineFileName="none" # yourMachinefileName
 logPath=$casePath
-logFileName="log_mpirun_$numberOfProcs_$solverName"
+logFileName="log_mpirun_$numberOfProcsCFD$solverName"
+
+# add post dir to DEM
+if [ ! -d "$casePath/DEM/post" ]; then
+  mkdir "$casePath/DEM/post"
+fi
+
+# add post dir to DEM
+if [ ! -d "$casePath/DEM/prev" ]; then
+  mkdir "$casePath/DEM/prev"
+fi
+
+reInitDEM="false"
+# if not exist restart file, execute DEM script to get restart file
+if [[ ! -f "$casePath/DEM/prev/restart.init" || $reInitDEM = "true" ]]; then
+  echo "execute DEM script to get restart file..."
+  cd $casePath/DEM
+  mpirun -np $numberOfProcsDEM liggghts < in.run
+else
+  echo "liggght: find restart file, no need to execute DEM script..."
+fi
 
 # block mesh
-# if [ -f "$casePath/CFD/constant/polyMesh/points" ]; then
-#   echo "blockMesh: mesh was built before and using old mesh"
-# fi
 if [ -d "$casePath/CFD/constant/polyMesh/" ]; then
   echo "blockMesh: remove mesh..."
   rm -r $casePath/CFD/constant/polyMesh/
@@ -40,18 +58,12 @@ echo "blockMesh: mesh will be built..."
 cd $casePath/CFD
 blockMesh
 
-# add post dir to DEM
-if [ ! -d "$casePath/DEM/post" ]; then
-  mkdir "$casePath/DEM/post"
-fi
-
 # call function to run a parallel CFD-DEM case
-mspCfdemParallelRun $casePath "$solverDir/$solverName" $numberOfProcs $machineFileName $logPath $logFileName
+mspCfdemParallelRun $casePath "$solverDir/$solverName" $numberOfProcsCFD $machineFileName $logPath $logFileName
 
 # define variables for post-process
 runOctave="false"
-postProc="false"
-liggghtsDumpFileName="dump.liggghts_run"
+postProc="true"
 
 if [ $runOctave = "true" ]; then
   echo
@@ -63,10 +75,21 @@ if [ $postProc = "true" ]; then
   # get VTK data from liggghts dump file
   echo
   cd $casePath/DEM/post
-  python2 $LPP_DIR/src/lpp.py $liggghtsDumpFileName
+  if [ ! -d "$casePath/DEM/post/coarse" ]; then
+    mkdir "$casePath/DEM/post/coarse"
+  fi
+  if [ ! -d "$casePath/DEM/post/fine" ]; then
+    mkdir "$casePath/DEM/post/fine"
+  fi
+  cp $casePath/DEM/post/dump_coarseParticles.run $casePath/DEM/post/coarse
+  cp $casePath/DEM/post/dump_fineParticles.run $casePath/DEM/post/fine
+  cd $casePath/DEM/post/coarse
+  python2 $LPP_DIR/src/lpp.py dump_coarseParticles.run
+  cd $casePath/DEM/post/fine
+  python2 $LPP_DIR/src/lpp.py dump_fineParticles.run
 fi
 
 #- keep terminal open (if started in new terminal)
 echo
-echo "press Ctrl+C kill process"
+echo "press Ctr+C kill process"
 read
